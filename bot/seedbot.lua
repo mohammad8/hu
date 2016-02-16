15f7952771975bf0c75894c0e919e6ff2887b964
Nitroteam <Tehran.021.2016@gmail.com>
2016-02-16 18:52:26 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -227,31 +227,14 @@ function create_config( )
     "invite",
     "all",
     "leave_ban",
-    "admin"
+    "admin",
+    "welcome"
     },
-    sudo_users = {120816252,147191022,(our_id)},--Sudo users
+    sudo_users = {120816252,147191022},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+admins : 
 ]],
     help_text_realm = [[
 Realm Commands:
