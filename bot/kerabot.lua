8ac672e160c8aeaf4dc3ef677071d905e70889d6
Nitroteam <Tehran.021.2016@gmail.com>
2016-02-23 15:23:48 +0330

    Update kerabot.lua

--- bot/kerabot.lua
+++ bot/kerabot.lua
@@ -239,19 +239,16 @@ function create_config( )
     "plugins",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {120816252,147191022},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
+    about_text = [[Kerabot v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
 
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
+@KeraBoy (developer)
+@mohammadslayer (helper)
 
 Special thanks to
 awkward_potato
@@ -260,8 +257,7 @@ topkecleon
 Vamptacus
 
 Our channels
-@teleseedch [English]
-@iranseed [persian]
+@kerach
 ]],
     help_text_realm = [[
 Realm Commands:
